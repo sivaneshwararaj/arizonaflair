93215760f07af014b6af9def2310b9f157bbc6c8
sivaneshwararj <sivaneshwararj@gmail.com>
2024-01-10 10:30:59 +0530

    ADDED FULL DESCRIPTION

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Arizona Flair',
+  title: 'Arizona Flair-Rent a waving tube man',
   description: 'Rent a waving tube man ',
 }
 
