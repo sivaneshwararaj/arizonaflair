213a33b620bc646cb9615963d10b2ec6547d57b9
sivaneshwararj <sivaneshwararj@gmail.com>
2024-01-10 10:31:42 +0530

    ADDED FULL DESCRIPTION

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Arizona Flair-Rent a waving tube man',
-  description: 'Rent a waving tube man ',
+  description: 'Rent a waving tube man',
 }
 
 export default function RootLayout({
